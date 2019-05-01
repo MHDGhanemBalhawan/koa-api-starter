51b49111a7155a35b956633671c1be4a92a129e5
Steve <mhdghanembalhawan@hotmail.com>
2019-05-01 11:06:56 +0100

    added details to api-project.js

--- public/api_project.js
+++ public/api_project.js
@@ -1,16 +1,16 @@
 define({
-  "name": "Job Portal API Documentation",
-  "version": "0.1.0",
-  "description": "apiDoc",
-  "title": "Job Portal",
-  "url": "https://",
-  "sampleUrl": false,
-  "defaultVersion": "0.0.0",
-  "apidoc": "0.3.0",
-  "generator": {
-    "name": "apidoc",
-    "time": "2019-04-27T14:09:10.607Z",
-    "url": "http://apidocjs.com",
-    "version": "0.17.7"
+  name: "Job Portal API Documentation",
+  version: "0.1.0",
+  description: "Job Portal API by MHD Ghanem Balhawan",
+  title: "Job Portal",
+  url: "https://fathomless-falls-28715.herokuapp.com/",
+  sampleUrl: false,
+  defaultVersion: "0.0.0",
+  apidoc: "0.3.0",
+  generator: {
+    name: "apidoc",
+    time: "2019-04-27T14:09:10.607Z",
+    url: "http://apidocjs.com",
+    version: "0.17.7"
   }
 });
