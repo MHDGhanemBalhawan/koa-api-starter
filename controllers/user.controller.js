66c0cafe4406e850d3681d90e32de5613b30571d
Steve <mhdghanembalhawan@hotmail.com>
2019-05-01 13:00:26 +0100

    update the apiDoc url

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -2,7 +2,7 @@ const UtilService = require("../services/util.service");
 const JwtService = require("../services/jwt.service");
 module.exports = {
   /**
-   * @api {post} signup
+   * @api {post} signup Signup unique email and password
    * @apiGroup Users
    * @apiName signupUser
    * @apiParam {String} [email] user must enter email
